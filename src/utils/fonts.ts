9f11437ef290d4b40745440d3df5ef126d511041
Ruzil357 <rushilgupta4@gmail.com>
2023-11-03 23:01:12 +0530

    Added home page

---
+++ src/utils/fonts.ts
@@ -0,0 +1,13 @@
+import { Poppins, Bayon } from 'next/font/google';
+
+export const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const bayon = Bayon({
+  subsets: ['latin'],
+  weight: '400',
+});
