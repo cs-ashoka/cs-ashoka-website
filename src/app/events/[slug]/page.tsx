23b368d2ac070855f33ceb01553f5f4244962c6a
vanshbothra <67708601+vanshbothra@users.noreply.github.com>
2024-02-08 04:56:08 +0530

    add masonry grid

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import HeroImage from "../../../../public/img/aboutbg.png";
 import { getSortedPostsData } from "@/lib/event-posts";
 import { getPostData } from "@/lib/event-posts";
-import { GalleryWithCarousel } from "@/components/carousel/carousel";
+import { MasonryGridGallery } from "@/components/carousel/carousel";
 import NotFound from "@/app/not-found";
 import { imageOptimizer } from "next/dist/server/image-optimizer";
 
@@ -18,7 +18,7 @@ import { imageOptimizer } from "next/dist/server/image-optimizer";
 // }
 
 // export function generateMetadata({ params }: { params: { eventSlug: string } }) {
-    
+
 //     const eventPosts = getSortedPostsData()
 //     const { eventSlug } = params
 
@@ -37,51 +37,53 @@ import { imageOptimizer } from "next/dist/server/image-optimizer";
 //     }
 // }
 
-export default async function EventsPost( { params }: { params: {slug: string } }) {
+export default async function EventsPost({ params }: { params: { slug: string } }) {
 
-    const eventPosts = getSortedPostsData()
+  const eventPosts = getSortedPostsData()
 
-    if (!eventPosts.find(event => event.slug === params.slug)) NotFound()
+  if (!eventPosts.find(event => event.slug === params.slug)) NotFound()
 
-    const { title, date, contentHtml } = await getPostData(params.slug)
+  const { title, date, contentHtml } = await getPostData(params.slug)
 
-    // const eventDate = getFormattedDate(date)
+  // const eventDate = getFormattedDate(date)
 
-    return (
-      <main className={`w-full h-fit overflow-x-hidden`}>
-        <div className="-z-10 bottom-0 absolute w-full h-[86.5vh] overflow-hidden">
-          <span className="block w-full h-full relative overflow-hidden">
-            <Image
-              src={HeroImage}
-              priority
-              placeholder="blur"
-              alt="Person coding"
-              fill={true}
-              quality={100}
-              className="object-cover object-bottom w-full h-full"
-            />
-          </span>
+  return (
+    <main className={`w-full h-fit overflow-x-hidden`}>
+      <div className="-z-10 bottom-0 absolute w-full h-[86.5vh] overflow-hidden">
+        <span className="block w-full h-full relative overflow-hidden">
+          <Image
+            src={HeroImage}
+            priority
+            placeholder="blur"
+            alt="Person coding"
+            fill={true}
+            quality={100}
+            className="object-cover object-bottom w-full h-full"
+          />
+        </span>
+      </div>
+      {/* <div className={`absolute ml-1/5`}>/</div> */}
+      <div className={`md:w-3/5 w-5/6 mx-auto mb-10`}>
+        <div className={`md:bg-white bg-primary px-10 py-3 rounded-2xl w-full mx-auto md:mt-[4vh]`}>
+          <p
+            className={`${bayon.className} md:text-primary text-white text-5xl md:text-8xl font-bold text-center`}
+          >
+            {title}
+          </p>
+          <p className={`${poppins.className} text-black md:text-4md font-bold text-center`}>
+            {date}
+          </p>
         </div>
-        {/* <div className={`absolute ml-1/5`}>/</div> */}
-        <div className={`md:w-3/5 w-5/6 mx-auto mb-10`}>
-            <div className={`md:bg-white bg-primary px-10 py-3 rounded-2xl w-full mx-auto md:mt-[4vh]`}>
-                <p
-                className={`${bayon.className} md:text-primary text-white text-5xl md:text-8xl font-bold text-center`}
-                >
-                {title}
-                </p>
-                <p className={`${poppins.className} text-black md:text-4md font-bold text-center`}>
-                    {date}
-                </p>
-            </div>
-            <div className={`bg-white py-5 px-10 rounded-2xl max-md:text-center mt-8`}>
-                <section className={`${poppins.className} text-black md:text-4md text-justify`} dangerouslySetInnerHTML={{ __html: contentHtml }} />
-            </div>
-            <br></br>
-            <GalleryWithCarousel />
+      </div>
+      <div className={`w-11/12 mx-auto mb-10`}>
+        <div className={`bg-white py-5 px-10 rounded-2xl max-md:text-center mt-8`}>
+          <section className={`${poppins.className} text-black md:text-4md text-justify`} dangerouslySetInnerHTML={{ __html: contentHtml }} />
         </div>
-      </main>
-    );
-  };
+        <br></br>
+        <MasonryGridGallery />
+      </div>
+    </main>
+  );
+};
 
 // export default EventsPost;
--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -2,26 +2,94 @@
 
 import { ThemeProvider, Carousel } from "@material-tailwind/react";
 
-export function GalleryWithCarousel() {
-    return (
-      <div className="grid grid-cols-2 grid-rows-2 gap-2">
-        <div className="row-span-2">
-        <img
-          src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover object-center"
-        /> 
+export function MasonryGridGallery() {
+  return (
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            alt="gallery-photo"
+          />
         </div>
-        <img
-          src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-          alt="image 2"
-          className="h-full w-full object-cover object-center"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="image 3"
-          className="h-full w-full object-cover object-center"
-        />
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center "
+            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+            alt="gallery-photo"
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            alt="gallery-photo"
+          />
+        </div>
       </div>
-    );
-  }
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt="gallery-photo"
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            alt="gallery-photo"
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center "
+            src="https://docs.material-tailwind.com/img/team-3.jpg"
+            alt="gallery-photo"
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            alt="gallery-photo"
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center "
+            src="https://docs.material-tailwind.com/img/team-3.jpg"
+            alt="gallery-photo"
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt="gallery-photo"
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt="gallery-photo"
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+            alt="gallery-photo"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
