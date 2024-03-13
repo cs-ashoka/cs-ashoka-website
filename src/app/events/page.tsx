a54c4b79053e56d8b162323d7be1a257d2290f71
manyagarggg <mgmanyagarg1710@gmail.com>
2024-03-13 23:39:50 +0530

    minor change

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -57,11 +57,6 @@ const EventsPage = () => {
       <div className={`mx-auto w-5/6 md:w-4/5 mb-10 md:mb-0 mt-4`}>
       <MasonryGridGallery />
       </div>
-      <footer className={`mt-8 rounded-t-[30px] md:rounded-t-[20px] p-4 px-[40px] md:px-[60px] bg-primary mt-4`}>
-                <p className={`${poppins.className} text-center line-height-[45px] text-xl mb-2 text-tertiary`}>Contact Us</p>
-                <div className={`flex items-center gap-2 -mb-2 md:-mb-0 md:gap-x-4 gap-x-8`}>
-                    </div>
-                </footer>
     </main>
   );
 };
